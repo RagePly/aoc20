cf6c659698d5bc9db40a2719cce64cfc2e3045df
Hugo Lom <the.hugo.lom@gmail.com>
2020-12-01 07:32:55 +0100

    add solution 1 back into source

--- day1.py
+++ day1.py
@@ -1,11 +1,14 @@
-tmp = open("../aoc20data/day1.txt", "r").readlines()
+tmp = open("../aoc20data/data1.txt", "r").readlines()
 flag = False
 for expense in tmp:
     for expense2 in tmp:
+        sum2 = int(expense) + int(expense2)
+        if sum2 == 2020:
+            print("Day1: Product {}*{}={}".format(expense.strip(),expense2.strip(),int(expense)*int(expense2)))
         for expense3 in tmp:
             sum3 = int(expense) + int(expense2) + int(expense3)
             if sum3 == 2020:
-                print("Product {}*{}*{}={}".format(expense.strip(),expense2.strip(),expense3.strip(),int(expense)*int(expense2)*int(expense3)))
+                print("Day2: Product {}*{}*{}={}".format(expense.strip(),expense2.strip(),expense3.strip(),int(expense)*int(expense2)*int(expense3)))
                 flag = True
                 break
         if flag:
