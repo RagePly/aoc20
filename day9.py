889317156c6229e76aa6cf3f1df6e8972ac55151
Hugo Lom <the.hugo.lom@gmail.com>
2020-12-09 08:43:21 +0100

    day 9 entry

---
+++ day9.py
@@ -0,0 +1,31 @@
+# TASK 1 ========================================================================
+with open("data/day9.txt","r") as f:
+    data = [int(x) for x in f.readlines()]
+
+preample_len = 25
+
+for i in range(preample_len,len(data)):
+    z = data[i]
+    flag = False
+    for x in data[i-preample_len:i]:
+        for y in data[i-preample_len:i]:
+            if (not x == y) and (x + y == z):
+                flag = True
+                break
+        if flag:
+            break
+    else:
+        invalid = z
+        print("First invalid: {}".format(z))
+        break
+
+# TASK 2 ========================================================================
+for i in range(0,len(data)):
+    rgn = [data[i]]
+    for j in range(i+1, len(data)):
+        rgn.append(data[j])
+        if sum(rgn) > invalid:
+            break
+        elif sum(rgn) == invalid:
+            print("Weakness: {}".format(min(rgn) + max(rgn)))
+            quit()
