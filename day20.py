55a2300b350a54a87722d8cf2be2b2acc5a0aa3b
Hugo Lom <the.hugo.lom@gmail.com>
2020-12-22 00:22:52 +0100

    completed day 20, task 2 on day 21

--- day20.py
+++ day20.py
@@ -42,227 +42,187 @@ for c in itertools.permutations(data.keys(),2):
 # Corners have two neighbours
 print(math.prod([k for k in data.keys() if len(pairs[k].keys()) == 2]))
 
-# TASK 2 NOT COMPLETED ======================================================================================================
+# TASK 2 COMPLETED ON DAY 21 =================================================================================================
+import re
 import itertools
 import math
-with open("data/day20.txt", "r") as f:
-    _data = [x.strip() for x in f.readlines()]
 
-len_data = len(_data)
-data = {}
-flag = True
-while flag:
-    i1 = _data[0].find(" ")
-    i2 = _data[0].find(":")
-    tile_nr = int(_data[0][i1+1:i2])
+# Def helper functions
+toMapArr = lambda map_str, num: [map_str[i:i+num] for i in range(0,len(map_str), num)]
+toMapStr = lambda map_arr: "".join(map_arr)
+toStr = lambda map_arr: "".join([l + "\n" for l in map_arr])
 
-    if "" in _data:
-        i3 = _data.index("")
-        img_data = _data[1:i3]
+# If axis = y, will flip so that x' = -x, else y'=-y
+flipMap = lambda map_arr, axis="y" : [l[::-1] for l in map_arr] if axis == "y" else map_arr[::-1]
+
+def rotateMapCC(map_arr, nr_rotations):
+    """Rotate counter clockwise the given number of steps"""
+    new_map_arr = map_arr
+    side_len = len(new_map_arr)
+    for _ in range(nr_rotations):
+        new_map_arr = ["".join([x[i] for x in new_map_arr]) for i in range(side_len).__reversed__()]
+    return new_map_arr
+
+def getSide(map_arr, side):
+    """Side is an index that corresponds to the directions in list [right, up, left, down]"""
+    sides = ["r","u","l","d"]
+
+    if sides[side] == "u":
+        return map_arr[0][::-1]
+    elif sides[side] == "d":
+        return map_arr[-1]
+    elif sides[side] == "l":
+        return "".join([l[0] for l in map_arr])
     else:
-        img_data = _data[1:]
-        flag = False
+        return "".join(l[-1] for l in map_arr.__reversed__())
 
-    l_border = "".join([x[0] for x in img_data])[::-1] # ^
-    r_border = "".join([x[-1] for x in img_data]) # v
-    u_border = img_data[0] # ->
-    d_border = img_data[-1][::-1] # <-
+getSides = lambda map_arr: [getSide(map_arr, i) for i in range(4)]
 
-    #print(tile_nr,l_border,r_border,u_border,d_border)
-    data.update({tile_nr:{"img":img_data, "l":l_border, "r":r_border, "u":u_border, "d":d_border}})
-    _data = _data[i3+1:]
+def rec_build(tile_index, set_index, tile_data ,coord=[0,0], visited=set(), tile_map={}, rotate=0, flip=None):
+    """Recursive build function, supply with starting index, the set of all indexes as well as a dict of {index:data}"""
+    # Update set of visited indices with the current one
+    visited.update({tile_index})
 
-comb_borders = list(itertools.product(["l","r","u","d"], repeat=2))
-pairs = {}
-for c in itertools.permutations(data.keys(),2):
-    compare = [(b, False) for b in comb_borders if data[c[0]][b[0]] == data[c[1]][b[1]][::-1]]
-    compare += [(b, True) for b in comb_borders if data[c[0]][b[0]] == data[c[1]][b[1]]]
-    if compare:
-        if not c[0] in pairs:
-            pairs.update({c[0]:{c[1]:compare}}) 
-        else:
-            pairs[c[0]].update({c[1]:compare})
-
-# Rotate function
-def rotateImg(imgData,rotateStep):
-    newImg = imgData.copy()
-    for _ in range(rotateStep):
-        newImg = ["".join([line[i] for line in newImg]) for i in range(len(newImg)).__reversed__()]
-    return newImg
-
-# Flip function
-def flipImg(imgData, flipAxis):
-    if flipAxis == "y":
-        newImg = [l[::-1] for l in imgData]
-    else:
-        newImg = imgData[::-1]
-    return newImg
-
-rotateScheme = {
-    ('l', 'l'):2,
-    ('l', 'r'):0,
-    ('l', 'u'):3,
-    ('l', 'd'):1,
-    ('r', 'l'):0,
-    ('r', 'r'):2,
-    ('r', 'u'):1,
-    ('r', 'd'):3,
-    ('u', 'l'):1,
-    ('u', 'r'):3,
-    ('u', 'u'):2,
-    ('u', 'd'):0,
-    ('d', 'l'):3,
-    ('d', 'r'):1,
-    ('d', 'u'):0,
-    ('d', 'd'):2
-}
-
-rotateOrder = ["l","d","r","u"]
-
-flipSheme = lambda x: "y" if x in ["u", "d"] else "x"
-
-flipDir = {
-    "u":"d",
-    "d":"u",
-    "l":"r",
-    "r":"l"
-}
-
-visitedIds = {}
-
-def recursive_build(currentIndex, currentCoord=[0,0], flip=False, pathHere=None):
-    print("{} [".format(currentIndex))
-    cImg = data[currentIndex]["img"]
-    visitedIds.update({currentIndex:{}})
-    coord = currentCoord.copy()
-    rotation = 0
-    flipx = False # Flip positions of l and r (on the x axis)
-
+    # If any rotation or flip was required to fit the last tile, apply those to the image data
+    current_map_arr = tile_data[tile_index]
     if flip:
-        flipx = True if flipSheme(pathHere[1]) == "x" else False
-        cImg = flipImg(cImg,flipSheme(pathHere[1]))
+        current_map_arr = flipMap(current_map_arr,flip)
+    current_map_arr = rotateMapCC(current_map_arr,rotate)
+
+    # The tile map that is recursivly built is updated with the current index, coordinate and new image data
+    tile_map.update({tile_index:(coord, current_map_arr)})
+
+    # If all tiles has been fitted, return the completed tile-map...
+    if set_index == visited:
+        return
     
-    if pathHere:
-        rotation = rotateScheme[pathHere]
-        cImg = rotateImg(cImg,rotation)
+    # ...otherwise get a list of each side of the current tile to fit the next. Only search for tiles
+    # that has not yet been fitted. 
+    current_sides = getSides(current_map_arr)
+    for otherIndex in set_index - visited:
+        # Since the visited-set is shared, updates further down the recursive path will be made available.
+        # I don't want to do uneccessary work and re-evaluate a tile (which would be tagged as not-visited
+        # by the time the set_index - visited expression was called).
+        if otherIndex in visited:
+            continue
 
-    print("""Came from: {}
-Current Coordinate: {}
-Has been flipped: {}
-Flipped along x-axis: {}
-Rotated: {}steps
-Starting recursion:""".format(pathHere, currentCoord, flip, flipx,rotation))
-    # Look here for errors, but this might be necessary
-    cImg = flipImg(cImg,"x")
-    visitedIds[currentIndex].update({"coord":coord, "img":cImg, "flip":flip, "flipx":flipx, "rotate":rotation})
-    for ids in pairs[currentIndex].keys():
-        if not ids in visitedIds:
-            flipNext = pairs[currentIndex][ids][0][1] ^ flip
-            newCoord = coord.copy()
-            prevLeg = pairs[currentIndex][ids][0][0][0]
-            print("""    Evaluating: {}
-    Will be flipped: {}
-    Previous connection: {}""".format(ids, flipNext, (prevLeg,pairs[currentIndex][ids][0][0][1])))
+        # Get the sides from the other tile and if any side mathces, note if a flip is needed and which sides
+        # of the two tiles matched
+        other_sides = getSides(tile_data[otherIndex])
+        # Match without flip
+        match = [(i[0],i[1], False) 
+            for i in itertools.product(range(4),repeat=2) 
+            if current_sides[i[0]] == other_sides[i[1]][::-1]]
+        if not match:
+            # Match with flip
+            match = [(i[0], i[1], True) 
+                for i in itertools.product(range(4),repeat=2) 
+                if current_sides[i[0]] == other_sides[i[1]]]
+
+        if match:
+            this_s, other_s, flip= match[0]
+            # Find around which axis to flip the other tile 
             if flip:
-                _prevleg = prevLeg
-                if flipx and prevLeg in ["r","l"]:
-                    prevLeg = flipDir[prevLeg]
-                elif not flipx and prevLeg in ["u", "d"]:
-                    prevLeg = flipDir[prevLeg]
-                
-                print("    Flipped {} - > {}".format(_prevleg, prevLeg))
-            
-            newLeg = rotateOrder[(rotateOrder.index(prevLeg) + rotation) % 4]
-            pathThere = (newLeg, pairs[currentIndex][ids][0][0][1])
-
-            print("""    New leg: {}
-    New Path: {}""".format(newLeg, pathThere))
-
-            if newLeg == "r":
-                newCoord[0] += 1
-            elif newLeg == "l":
-                newCoord[0] -= 1
-            elif newLeg == "u":
-                newCoord[1] += 1
+                flip = "y" if other_s in [1,3] else "x"
             else:
-                newCoord[1] -= 1
+                flip = None
+
+            # Find next coordinate based on which side of the current tile that matched.
+            # Coordinate are defined as [>,v] or [x,y]   
+            new_coord = coord.copy()
+            if this_s == 0:
+                new_coord[0] += 1
+            elif this_s == 1:
+                new_coord[1] -= 1
+            elif this_s == 2:
+                new_coord[0] -= 1
+            else:
+                new_coord[1] += 1
+
+            # Now that a rotation and a translation is determined, recursivly call the function again
+            # with the updated values. The (6 + side1 - side2) % 4 expression determines the number of
+            # counter-clockwise rotations needed to match the two sides.
+            rec_build(otherIndex, set_index, tile_data, 
+                    coord=new_coord, visited=visited, 
+                    tile_map=tile_map,rotate=(6+this_s-other_s)%4,
+                    flip=flip)
+
+    # Since the tile-map-dict is shared, it can simply be returned since only the return of top process will be 
+    # assigned to a variable 
+    return tile_map
             
-            print("    Next coordinate: {}\n{} > ".format(newCoord,currentIndex), end="")
+# Solution
+with open("data/day20.txt", "r") as f:
+    data = "".join([x.strip() for x in f.readlines()])
 
-            recursive_build(ids, currentCoord=newCoord, flip=flipNext, pathHere=pathThere)
-    print("Recursion end.\n] {}".format(currentIndex))
-            
+# Find all tiles according the format File INDEX: DATA. Tried to use the ":="" operator for the first time...
+tiles = {int((y := x.split(":"))[0]):toMapArr(y[1],10) 
+            for x in re.findall(r"[0-9]{4}:[.#]{100}",data)}
 
-recursive_build(list(data.keys())[0])
+# Create sets and starting value for the recursive function
+first_index = list(tiles.keys())[0]
+set_index = set(tiles.keys())
 
-# Build string
+# Build tileset with relative coordinates, find offset if top-left is [0,0]
+tile_map = rec_build(first_index, set_index, tiles)
+offsetx = - min([v[0][0] for v in tile_map.values()])
+offsety = - min([v[0][1] for v in tile_map.values()])
 
-## Find max offset
-minx = min([v["coord"][0] for v in visitedIds.values()])
-miny = min([v["coord"][1] for v in visitedIds.values()])
+# Create image using the coordinates found
+width = int(math.sqrt(len(tiles)))
+map_image = [[None for _ in range(width)] for _ in range(width)]
+for coord, img in tile_map.values():
+    map_image[coord[1] + offsety][coord[0] + offsetx] = list([l[1:-1] for l in img[1:-1]])
 
-offsetx = - minx
-offsety = - miny
+# Convert to a big tile
+map_whole = []
+for row in map_image:
+    for i in range(8):
+        map_whole.append("".join([col[i] for col in row]))
 
-print(offsetx, offsety)
+# Create monster regexp
+image_width = len(map_whole)
+monster_l = image_width - len("#....##....##....###")
+monstr_str = "".join([s + "."*monster_l for s in ["#.","#....##....##....###"]] + [".#..#..#..#..#..#"])
+monstr_re = re.compile(monstr_str)
 
-width = int(math.sqrt(len(data.keys())))
+# Create all map combinations to try
+map_variants = itertools.product([True, False], range(4))
 
-scatterplot = [["." for _ in range(20)] for _ in range(20)]
-for k,v in visitedIds.items():
-    scatterplot[v["coord"][1] + offsety][v["coord"][0] + offsetx] = "#"
+# Look through each map combination
+for f, r in map_variants:
+    # Apply rotation and ev. flip
+    if f:
+        map_var = rotateMapCC(flipMap(map_whole),r)
+    else:
+        map_var = rotateMapCC(map_whole,r)
 
-[print("".join(l)) for l in scatterplot]
-## Create array
-idpos = [[0 for _ in range(width)] for _ in range(width)]
+    # Create a string to search in
+    map_str = toMapStr(map_var)
 
-for k,v in visitedIds.items():
-    idpos[v["coord"][1] + offsety][v["coord"][0] + offsetx] = k
+    # Monsters are close to each other and therefore only the first will be found through regexp 
+    # => replace their textures and look again until no other is found. Only print result IF
+    # at least one monster has been found.
+    found_any = False
+    while True:
+        # Find monsters through regexp and count how many
+        monstrs = re.findall(monstr_re,map_str)
+        nr_monsters = len(monstrs)
+        if nr_monsters:
+            found_any = True
+            # Create a list of chars corresponding to map-str to replace monsters with "O".
+            map_list = list(map_str)
+            for m in monstrs:
+                i = map_str.find(m)
+                for i2 in range(len(monstr_str)):
+                    if monstr_str[i2] == "#":
+                        map_list[i + i2] = "O"
+            map_str = "".join(map_list)
+        elif found_any:
+            # At least one monster has been found and multiple searches has been made to find
+            # adjacent monsters, count number of "#" and print
+            print(map_str.count("#"))  
+            break
+        else:
+            break
 
-## Find image size 
-imgsize = len(visitedIds[idpos[0][0]]["img"])
-
-## Clear borders
-for v in visitedIds.values():
-    v["img"] = [x[1:-1] for x in v["img"][1:-1]]
-
-bigPicture = []
-for y in idpos:
-    for l in range(imgsize-2):
-        workingStr = ""
-        for x in y:
-            workingStr += visitedIds[x]["img"][l]
-
-        bigPicture.append(workingStr)
-
-bigFlipPicture = flipImg(bigPicture, "x")
-
-bigVar = [rotateImg(bigPicture,x) for x in range(4)]
-bigVar += [rotateImg(bigFlipPicture, x) for x in range(4)]
-
-import re
-
-monsterStr = ["..................#.","#....##....##....###", ".#..#..#..#..#..#..."]
-
-for i in range(len(monsterStr)-1):
-    monsterStr[i] += "."*(len(bigPicture) - len(monsterStr[i]))
-
-monsterStr = "".join(monsterStr)
-reMonster = re.compile(monsterStr)
-
-monsterMap = ""
-for m in bigVar:
-    mapStr = "".join(m)
-    monsters = re.findall(reMonster, mapStr)
-    if len(monsters) > 0:
-        monsterMap = mapStr
-        break
-
-monsterVision = list(monsterMap)
-for monster in monsters:
-    i = monsterMap.index(monster)
-    for i2 in range(len(monsterStr)):
-        if monsterStr[i2] == "#":
-            monsterVision[i+i2] = "O"
-
-print("".join(monsterVision).count("#"))
